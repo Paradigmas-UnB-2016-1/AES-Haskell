8a17aaf2cfa155817122022b493407e0f76ffb84
Eduardo Moreira <moreira.edu@hotmail.com>
2016-04-17 18:06:47 -0300

    Retirada função de multiplicação por polinomio 4x4 na substNibbles
    Signed-off-by: Eduardo Nunes <eduardonunes2525@gmail.com>
    Signed-off-by: Ebenezer Andrade <ebenezer.andrade1@gmail.com>

--- AES.hs
+++ AES.hs
@@ -25,47 +25,54 @@ main = do
 matrizToString matriz =
     concatBinario $ matriz!(1,1):matriz!(1,2):matriz!(2,1):matriz!(2,2):[]
 
---cifraMatrix matriz = addRoundKey(shiftRows(subtituteNibbles(addRoundKey(mixColumns(shiftRows(subtituteNibbles(addRoundKey(matriz))))))))
+--cifraMatrix matriz = addRoundKey(shiftRows(substituteNibbles(addRoundKey(mixColumns(shiftRows(substituteNibbles(addRoundKey(matriz))))))))
 
 --addRoundKey matriz =
 
-subtituteNibbles matriz =
-    array ((1, 1),(2,2)) [((1,1), finalXorSubstituteNibbles $ multiplicaPolinomio4por4 (pad4Bits (toDigits(matriz!(1,1))))),
-                          ((1,2), finalXorSubstituteNibbles $ multiplicaPolinomio4por4 (pad4Bits (toDigits(matriz!(1,2))))),
-                          ((2,1), multiplicaPolinomio4por4 (pad4Bits (toDigits(matriz!(2,1))))),
-                          ((2,2), multiplicaPolinomio4por4 (pad4Bits (toDigits(matriz!(2,2)))))]
+substituteNibbles :: (Num t, Num t1, Num t2, Num t3, Ix t, Ix t1, Ix t2, Ix t3) => Array (t2, t3) Integer -> Array (t, t1) [Integer]
+substituteNibbles matriz =
+    array ((1, 1),(2,2)) [((1,1), [0]),
+                          ((1,2), [0]),
+                          ((2,1), [0]),
+                          ((2,2), [0])]
 
-finalXorSubstituteNibbles bits =
-	bits!!0 `xor` 1 :
-    bits!!1 `xor` 0 :
-    bits!!2 `xor` 0 :
-    bits!!3 `xor` 1 :
-    []
+--finalXorSubstituteNibbles $ multiplicaPolinomio4por4 (pad4Bits (binToBinArray(matriz!(1,1))))
+--finalXorSubstituteNibbles bits =
+--	bits!!0 `xor` 1 :    
+--	bits!!1 `xor` 0 :    
+--	bits!!2 `xor` 0 :    
+--	bits!!3 `xor` 1 :    []
 
+pad4Bits :: Num a => [a] -> [a]
 pad4Bits xs = replicate (4 - length ys) 0 ++ ys
     where ys = take 4 xs
 
-toDigits = map (fromIntegral . digitToInt) . show
+binToBinArray :: Integer -> [Integer]
+binToBinArray = map (fromIntegral . digitToInt) . show
 
-multiplicaPolinomio4por4 bits = 
-    --concatBinario(
-    polinomio4por4!(1,1)*bits!!0 `xor` polinomio4por4!(1,2)*bits!!1 `xor` polinomio4por4!(1,3)*bits!!2 `xor` polinomio4por4!(1,4)*bits!!3 :
-    polinomio4por4!(2,1)*bits!!0 `xor` polinomio4por4!(2,2)*bits!!1 `xor` polinomio4por4!(2,3)*bits!!2 `xor` polinomio4por4!(2,4)*bits!!3 :
-    polinomio4por4!(3,1)*bits!!0 `xor` polinomio4por4!(3,2)*bits!!1 `xor` polinomio4por4!(3,3)*bits!!2 `xor` polinomio4por4!(3,4)*bits!!3 :
-    polinomio4por4!(4,1)*bits!!0 `xor` polinomio4por4!(4,2)*bits!!1 `xor` polinomio4por4!(4,3)*bits!!2 `xor` polinomio4por4!(4,4)*bits!!3 :
-    []
-    --)
+--multiplicaPolinomio4por4 :: [Integer] -> [Integer]
+--multiplicaPolinomio4por4 bits = 
+--    --concatBinario(
+--    polinomio4por4!(1,1)*bits!!0 `xor` polinomio4por4!(1,2)*bits!!1 `xor` polinomio4por4!(1,3)*bits!!2 `xor` polinomio4por4!(1,4)*bits!!3 :
+--    polinomio4por4!(2,1)*bits!!0 `xor` polinomio4por4!(2,2)*bits!!1 `xor` polinomio4por4!(2,3)*bits!!2 `xor` polinomio4por4!(2,4)*bits!!3 :
+--    polinomio4por4!(3,1)*bits!!0 `xor` polinomio4por4!(3,2)*bits!!1 `xor` polinomio4por4!(3,3)*bits!!2 `xor` polinomio4por4!(3,4)*bits!!3 :
+--    polinomio4por4!(4,1)*bits!!0 `xor` polinomio4por4!(4,2)*bits!!1 `xor` polinomio4por4!(4,3)*bits!!2 `xor` polinomio4por4!(4,4)*bits!!3 :
+--    []
+--    --)
 
+shiftRows :: (Num t, Num t1, Num t2, Num t3, Ix t, Ix t1, Ix t2, Ix t3) => Array (t2, t3) e -> Array (t, t1) e
 shiftRows matriz =
     array ((1, 1),(2,2))  [((1,1), matriz!(1,1)), ((1,2), matriz!(1,2)),
                             ((2,1), matriz!(2,2)), ((2,2), matriz!(2,1))]
 
+mixColumns :: (Num t, Num t1, Num t2, Num t3, Ix t, Ix t1, Ix t2, Ix t3) => Array (t2, t3) Integer -> Array (t, t1) Integer
 mixColumns matriz =
     array ((1, 1),(2,2)) [((1,1), multiplicaPolinomio2por2 (matriz!(1,1):matriz!(2,1):[])!(1,1)), 
                           ((1,2), multiplicaPolinomio2por2 (matriz!(1,2):matriz!(2,2):[])!(1,1)),
                           ((2,1), multiplicaPolinomio2por2 (matriz!(1,1):matriz!(2,1):[])!(2,1)),
                           ((2,2), multiplicaPolinomio2por2 (matriz!(1,2):matriz!(2,2):[])!(2,1))]
 
+multiplicaPolinomio2por2 :: (Num t, Num t1, Ix t, Ix t1) => [Integer] -> Array (t, t1) Integer
 multiplicaPolinomio2por2 coluna = 
     array ((1, 1),(2,1)) [((1,1), polinomio2por2!(1,1)*coluna!!0 + polinomio2por2!(1,2)*coluna!!1), 
                           ((2,1), polinomio2por2!(2,1)*coluna!!0 + polinomio2por2!(2,2)*coluna!!1)]
@@ -107,16 +114,13 @@ polinomio2por2 =
                           ((2,1), 4),
                           ((2,2), 1)]
 
-polinomio4por4 = 
-    array ((1, 1),(4,4)) [((1,1), 1), ((1,2), 0), ((1,3), 1), ((1,4), 1),
-                          ((2,1), 1), ((2,2), 1), ((2,3), 0), ((2,4), 1), 
-                          ((3,1), 1), ((3,2), 1), ((3,3), 1), ((3,4), 0),
-                          ((4,1), 0), ((4,2), 1), ((4,3), 1), ((4,4), 1)]
+--polinomio4por4 = 
+--    array ((1, 1),(4,4)) [((1,1), 1), ((1,2), 0), ((1,3), 1), ((1,4), 1),
+--                          ((2,1), 1), ((2,2), 1), ((2,3), 0), ((2,4), 1), 
+--                          ((3,1), 1), ((3,2), 1), ((3,3), 1), ((3,4), 0),
+--                          ((4,1), 0), ((4,2), 1), ((4,3), 1), ((4,4), 1)]
 
-teste1 =
-    multiplicaPolinomio4por4 [0,0,0,1]
-teste2 =
-    subtituteNibbles meuArray
+teste = substituteNibbles meuArray
 
 meuArray = 
     array ((1, 1),(2,2)) [((1,1), 1), 
