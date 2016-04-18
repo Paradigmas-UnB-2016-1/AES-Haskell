15da885bd607a7b574d35ce320413d7ebde63205
Eduardo Moreira <moreira.edu@hotmail.com>
2016-04-18 00:02:53 -0300

    Função AddRoundKey com chave fornecida
    Signed-off-by: Ebenezer Andrade <ebenezer.andrade1@gmail.com>
    Signed-off-by: Eduardo Nunes <eduardonunes2525@gmail.com>

--- AES.hs
+++ AES.hs
@@ -7,40 +7,51 @@ import Data.Char (digitToInt)
 
 --arquivoEntrada <- openFile "texto.txt" ReadMode
 
-bytestring = BC.pack (padTexto "dud")
+byteStringTexto = BC.pack (padTexto "dud")
 
-bytes = B.unpack bytestring
-chars = BC.unpack bytestring
+byteStringChave = BC.pack "6c8d"
+
+bytesTexto = B.unpack byteStringTexto
+charsTexto = BC.unpack byteStringTexto
+
+bytesChave = B.unpack byteStringTexto
 
 main = do
-    print bytes
-    print chars
-    print $ elems $ carregaMatriz (take 2 bytes)
+    print bytesTexto
+    print charsTexto
+--    cifraTexto bytesTexto
+    
 --  print (cifraTexto bytes)
 
--- cifraTexto [] = ""
---cifraTexto bytes =
---return (matrizToString (cifraMatriz (carregaMatriz (take 2 bytes))) ++ cifraTexto (drop 2 bytes))
+--cifraTexto [] = ""
+--cifraTexto bytesTexto =
+--    return (matrizToString cifraMatriz $ (carregaMatriz (take 2 bytesTexto)) (carregaMatriz (bytesChave)) ++ cifraTexto (drop 2 bytesTexto))
 
 --matrizToString matriz =
 --    concatBinario $ matriz!(1,1):matriz!(1,2):matriz!(2,1):matriz!(2,2):[]
 
+--cifraMatrix :: Array (t2, t3) Integer -> Array (t, t1) Integer -> Array (t, t1) Integer 
 --cifraMatrix matriz chave1 = 
---	let chave2 = expandir chave 
---	    chave3 = expandir chave2
---	in addRoundKey $ (chave3) (shiftRows $ substituteNibbles $ addRoundKey $ (chave2) (mixColumns $ shiftRows $ substituteNibbles $ addRoundKey $ chave matriz))
+--	let chave2 = expandir chave1 1
+--	    chave3 = expandir chave2 2
+--	in addRoundKey $ (chave3) (shiftRows $ substituteNibbles $ addRoundKey $ (chave2) (mixColumns $ shiftRows $ substituteNibbles $ addRoundKey $ chave1 matriz))
 
 addRoundKey chave matriz = 
-    array ((1, 1),(2,2)) [((1,1), realizaXorAddRoundKey (chave!(1,1)) (matriz!(1,1))),
-                          ((1,2), realizaXorAddRoundKey (chave!(1,2)) (matriz!(1,2))),
-                          ((2,1), realizaXorAddRoundKey (chave!(2,1)) (matriz!(2,1))),
-                          ((2,2), realizaXorAddRoundKey (chave!(2,2)) (matriz!(2,2)))]
+    array ((1, 1),(2,2)) [((1,1), realizaXorBitPorBit (binToBinArray8Bits $ chave!(1,1)) (binToBinArray8Bits $ matriz!(1,1))),
+                          ((1,2), realizaXorBitPorBit (binToBinArray8Bits $ chave!(1,2)) (binToBinArray8Bits $ matriz!(1,2))),
+                          ((2,1), realizaXorBitPorBit (binToBinArray8Bits $ chave!(2,1)) (binToBinArray8Bits $ matriz!(2,1))),
+                          ((2,2), realizaXorBitPorBit (binToBinArray8Bits $ chave!(2,2)) (binToBinArray8Bits $ matriz!(2,2)))]
 
-realizaXorAddRoundKey bitsChave bitsMatriz =
-    concatBinario $ (binToBinArray bitsChave)!!0 `xor` (binToBinArray bitsMatriz)!!0 : 
-                    (binToBinArray bitsChave)!!1 `xor` (binToBinArray bitsMatriz)!!1 : 
-                    (binToBinArray bitsChave)!!2 `xor` (binToBinArray bitsMatriz)!!2 : 
-                    (binToBinArray bitsChave)!!3 `xor` (binToBinArray bitsMatriz)!!3 : []
+--Função necessita receber duas listas com 8 elementos inteiros
+realizaXorBitPorBit bitsWa bitsWb' =
+    concatBinario $ (bitsWa)!!0 `xor` (bitsWb')!!0 : 
+                    (bitsWa)!!1 `xor` (bitsWb')!!1 : 
+                    (bitsWa)!!2 `xor` (bitsWb')!!2 : 
+                    (bitsWa)!!3 `xor` (bitsWb')!!3 :
+                    (bitsWa)!!4 `xor` (bitsWb')!!4 : 
+                    (bitsWa)!!5 `xor` (bitsWb')!!5 : 
+                    (bitsWa)!!6 `xor` (bitsWb')!!6 : 
+                    (bitsWa)!!7 `xor` (bitsWb')!!7 : []
 
 substituteNibbles matriz =
     array ((1, 1),(2,2)) [((1,1), substituiBitsSbox $ matriz!(1,1)),
@@ -52,8 +63,7 @@ substituiBitsSbox :: Integer -> Integer
 substituiBitsSbox bits = sBox!(binArrayToInt(take 2 $ binToBinArray(bits)) + 1, (binArrayToInt(drop 2 $ binToBinArray(bits))+1)) 
 
 binArrayToInt::[Integer] -> Integer 
-binArrayToInt binarys = 
-        binarys!!0*2 + binarys!!1*1
+binArrayToInt bits = bits!!0 * 2 + bits!!1 * 1
 
 --finalXorSubstituteNibbles $ multiplicaPolinomio4por4 (binToBinArray(matriz!(1,1)))
 --finalXorSubstituteNibbles bits =
@@ -66,8 +76,15 @@ pad4Bits :: Num a => [a] -> [a]
 pad4Bits xs = replicate (4 - length ys) 0 ++ ys
     where ys = take 4 xs
 
+pad8Bits :: Num a => [a] -> [a]
+pad8Bits xs = replicate (8 - length ys) 0 ++ ys
+    where ys = take 8 xs
+
 binToBinArray :: Integer -> [Integer]
 binToBinArray = pad4Bits . map (fromIntegral . digitToInt) . show
+
+binToBinArray8Bits :: Integer -> [Integer]
+binToBinArray8Bits = pad8Bits . map (fromIntegral . digitToInt) . show
 
 --multiplicaPolinomio4por4 :: [Integer] -> [Integer]
 --multiplicaPolinomio4por4 bits = 
@@ -118,10 +135,6 @@ calculaBinario 0 = []
 calculaBinario n | n `mod` 2 == 1 = 1 : calculaBinario (n `div` 2)
                  | n `mod` 2 == 0 = 0 : calculaBinario (n `div` 2)
 
-pad8Bits :: Num a => [a] -> [a]
-pad8Bits xs = replicate (8 - length ys) 0 ++ ys
-    where ys = take 8 xs
-
 padTexto :: [Char] -> [Char]
 padTexto texto = texto ++ replicate ((length texto) `mod` 2) ' '
 
@@ -153,10 +166,10 @@ sBox = array ((1,1),(4,4)) [((1,1), 1001), ((1,2), 0100), ((1,3), 1010), ((1,4),
 
 teste = substituteNibbles meuArray
 
-chave = array ((1, 1),(2,2)) [((1,1), 100), 
-                              ((1,2), 1011),
-                              ((2,1), 101),
-                              ((2,2), 1001)]
+chave = array ((1, 1),(2,2)) [((1,1), 0010), 
+                              ((1,2), 1101),
+                              ((2,1), 0101),
+                              ((2,2), 0101)]
 
 meuArray =  array ((1, 1),(2,2)) [((1,1), 1), 
                                   ((1,2), 11),
